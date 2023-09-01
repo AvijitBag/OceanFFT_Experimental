089e6f82d3c06b425000afc7b4a0824e06ba6008
Avijit Bag <90190061+AvijitBag@users.noreply.github.com>
2023-09-01 17:40:22 +0530

    Add files via upload

---
+++ Sycl_Migrated/Samples/4_CUDA_Libraries/oceanFFT/compile.sh
@@ -0,0 +1,2 @@
+icpx -fsycl -fsycl-targets=nvptx64-nvidia-cuda *.cpp -I../../../Common/ -I ../../../include/ -I /nfs/site/home/sselmax/Avijit/oneMKL/build/include -L /nfs/site/home/sselmax/Avijit/oneMKL/build/lib -lonemkl
+
